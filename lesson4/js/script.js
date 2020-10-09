c500f726b23c30a14f5f5031f66a0fd2a09115da
phjessee <p.berto99@gmail.com>
2020-10-08 22:23:59 -0600

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,6 @@
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+var x = document.lastModified;
+document.getElementById("currentDate").innerHTML = x;
