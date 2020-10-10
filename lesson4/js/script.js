6552b4415dcfa6c970f98f0e2c9a24b2dfe31364
phjessee <p.berto99@gmail.com>
2020-10-09 21:37:43 -0600

    commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,11 +1,6 @@
-const options = {weekday: 'long', day: "numberic", month: 'long', year: 'numberic'};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString ('en-US', options);
-
-
-
-var x = document.lastModified;
-document.getElementById("currentDate").innerHTML = x;
-
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+var x = document.lastModified;
+document.getElementById("currentDate").innerHTML = x;
