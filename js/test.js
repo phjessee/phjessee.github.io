2d9d7a56140152b2e6f747520d42228d6c8e8dca
phjessee <p.berto99@gmail.com>
2020-10-10 23:05:20 -0600

    updates

--- js/test.js
+++ js/test.js
@@ -1,2 +1,6 @@
 var x = document.lastModified;
 document.getElementById("currentDate").innerHTML = x;
+
+var n = new Date();
+var y = n.getFullYear();
+document.getElementById("currentYear").innerHTML = y;
