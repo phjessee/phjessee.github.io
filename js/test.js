a1519fa61abfd5b179721b94bd3ab293c513df2b
phjessee <p.berto99@gmail.com>
2020-10-16 09:44:48 -0600

    commit

--- js/test.js
+++ js/test.js
@@ -4,3 +4,5 @@ document.getElementById("currentDate").innerHTML = x;
 var n = new Date();
 var y = n.getFullYear();
 document.getElementById("currentYear").innerHTML = y;
+
+WebFont.load({ google: { families: ['Roboto']}});
