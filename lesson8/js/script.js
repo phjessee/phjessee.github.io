38ae9ea9b504bae76c0b87deb61e3e07243ea5fb
phjessee <p.berto99@gmail.com>
2020-11-03 08:48:45 -0700

    commit

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -1,43 +1,30 @@
 /*######## Menu Button #####*/
 
-function toggleMenu(){
+function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
 
 /*######## Footer Date #####*/
 try {
-    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
     document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
 } catch (e) {
     alert('Error with code or your browser does not support Locale');
 }
 
 
-/*######## Pancakes Banner #####*/
-
-var d = new Date();
-var x = d.getDay();
-
-if (x == 5) {
-    document.getElementById('message').style.display = "block";
-}
-else {
-   document.getElementById("message").style.display = "none";
-}
-
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
-
-
 /*##### Form Slider ####*/
 
 var slider = document.getElementById("severity");
-var output = document.getElementById("ouput");
-output.innerHTML = slider.value; // Display the default slider value
+var output = document.getElementById("output");
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
+slider.oninput = function () {
+    output.innerHTML = this.value;
 }
