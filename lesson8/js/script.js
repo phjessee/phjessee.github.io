b73cbab0769b917f3b1148a54054904168ad32b4
phjessee <p.berto99@gmail.com>
2020-11-06 13:08:20 -0700

    commit

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -7,17 +7,10 @@ function toggleMenu() {
 
 /*######## Footer Date #####*/
 try {
-    const options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    };
-    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en', options);
 } catch (e) {
-    alert('Error with code or your browser does not support Locale');
 }
-
 
 /*##### Form Slider ####*/
 
