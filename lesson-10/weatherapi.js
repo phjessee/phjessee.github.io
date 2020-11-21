a09eb1c15be9aaada9854281c1b5c01b98fb1fc6
phjessee <p.berto99@gmail.com>
2020-11-20 20:10:17 -0700

    commit

--- lesson-10/weatherapi.js
+++ lesson-10/weatherapi.js
@@ -16,8 +16,7 @@ fetch(apiURL)
         if (temp >= 50 && speed >= 3) {
             let f = (35.74 + (0.6215 * t)) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * (t * (Math.pow(speed, 0.16))));
             chill = Math.round(f);
-        }
-        else {
+        } else {
             chill = "N/A";
         }
     });
@@ -33,17 +32,17 @@ fetch(forecastURL)
         const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
         for (let day = 0; day < forecast.length; day++) {
+
             const d = new Date(forecast[day].dt_txt);
-            const imgSrc = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '@1x.png';
+            const imgSrc = 'https://openweathermap.org/img/w/' + forecast[day].weather[0].icon + '.png';
             const desc = forecast[day].weather[0].description;
+
 
             document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
             document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast[day].main.temp);
             document.getElementById(`icon${day+1}`).setAttribute('src', imgSrc);
             document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+
         }
 
     });
-
-
-    
