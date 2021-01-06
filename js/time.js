7a1b8f43f2bf1c379c7566c9d55616074ee88fb3
phjessee <p.berto99@gmail.com>
2021-01-06 13:34:07 -0700

    commit

--- js/time.js
+++ js/time.js
@@ -2,6 +2,9 @@ const options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
-    year: 'numeric'
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
 };
 document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
