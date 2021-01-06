7f8b6aed45d3431e2be62d1618fe535b2eedf06f
phjessee <p.berto99@gmail.com>
2021-01-06 13:38:08 -0700

    udpate

--- js/time.js
+++ js/time.js
@@ -1,10 +1,2 @@
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-};
-document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+const modified = document.lastModified;
+document.getElementById('currentDate').textContent = modified;
