ab2cfb410b8463cfaf519fc354a9ce67b5c55db7
phjessee <p.berto99@gmail.com>
2021-01-29 13:50:03 -0700

    commit

---
+++ lesson4/js/script.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+try {
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+} catch (e) {
+    alert('Error with code or your browser does not support Locale');
+}
