3082b234eac56614d266bed2c9d1b29405254044
phjessee <p.berto99@gmail.com>
2021-03-15 19:31:25 -0600

    commit

--- lesson-11/js/preston-events.js
+++ lesson-11/js/preston-events.js
@@ -1,3 +1,23 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+  }
+
+
+    /*######## Footer Date #####*/
+try {
+    const options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    };
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
+  }
+
+
+
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 function events(city) {
